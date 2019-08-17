ab274350cdc1d2d8e141c59999910f2e284005d1
Rajendran <rajendran1166@gmail.com>
2019-08-17 10:44:39 +0530

    Added output of Docker and Kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,11 +8,14 @@
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run my-app --image=rajendran1166/prediction --port=8000
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+>kubectl port-forward my-app-75f6fd5c6f-ftr5h 8000:8000
 
